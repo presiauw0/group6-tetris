f7ecc0dbbdf3e835c5a320dcb4dbb2cb62ec65c8
presia27 <pre.sia977@outlook.com>
2024-11-14 15:16:20 -0800

    Added JavaDoc and a logger. Logging does not work right now.

--- src/edu/uw/tcss/app/SandBox.java
+++ src/edu/uw/tcss/app/SandBox.java
@@ -1,11 +1,39 @@
 package edu.uw.tcss.app;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Board;
 
-public class SandBox {
 
+/**
+ * The sandbox class for testing Git.
+ *
+ * @author Preston Sia (305 Group 6)
+ * @version F2024_001
+ */
+public final class SandBox {
+
+    /**
+     * Logger object for logging info and issues with this code.
+     */
+    public static final Logger LOGGER = Logger.getLogger(SandBox.class.getName());
+
+    private SandBox() {
+        super();
+    }
+
+    static {
+        // Level.ALL - Display ALL logging messages
+        // Level.OFF - Display NO logging messages
+        LOGGER.setLevel(Level.ALL);
+    }
+
+    /**
+     * The main driver method for this class.
+     * @param theArgs Argument from the command line.
+     */
     public static void main(final String[] theArgs) {
-        Board b = new Board();
+        final Board b = new Board();
         b.newGame();
         System.out.println(b);
 

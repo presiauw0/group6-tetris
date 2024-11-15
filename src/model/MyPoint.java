79d795f50d33916a27cb453988b4c0806b0660a1
ahhassan <ahhassan@uw.edu>
2024-11-14 22:46:11 -0800

    I created the interface for the Point class.

---
+++ src/model/MyPoint.java
@@ -0,0 +1,41 @@
+package model;
+/**
+ * Interface representing a 2D point with integer coordinates.
+ * Provides methods to retrieve the coordinates of the point
+ * and transform the point's position.
+ *
+ * @author Abdulrahman Hassan
+ * @version Autumn 2024
+ *
+ */
+public interface MyPoint {
+
+    /**
+     * Returns the x-coordinate of the point.
+     *
+     * @return the x-coordinate of the point.
+     */
+    int x();
+
+    /**
+     * Returns the y-coordinate of the point.
+     *
+     * @return the y-coordinate of the point.
+     */
+    int y();
+
+    /**
+     * Transforms the point to a new position specified by the given x and y coordinates.
+     *
+     * @param theX the new x-coordinate of the point.
+     * @param theY the new y-coordinate of the point.
+     */
+    void transform(int theX, int theY);
+
+    /**
+     * Transforms the point to the position of another MyPoint object.
+     *
+     * @param thePoint the MyPoint object whose coordinates will be copied.
+     */
+    void transform(MyPoint thePoint);
+}
